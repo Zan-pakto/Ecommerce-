49b6011c1dd8c81c9e542a0896a77a45b2a2db37
Arvind Shahi <150140351+Zan-pakto@users.noreply.github.com>
2025-03-05 18:50:30 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ mongoose.connect(
   "mongodb+srv://arvind:Shahi1234@cluster0.9xbts.mongodb.net/Ecommerce"
 );
 const userSchema = new mongoose.Schema({
+  
   username: String,
   email: String,
   password: String,
